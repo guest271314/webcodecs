04cb39cc074531775d68cd38a1dc49ac5df9051b
guest271314 <guest271314@gmail.com>
2021-07-31 18:19:57 -0700

    Update audioencoder-audiodecoder-mediastreamtrackgenerator.js

--- audioencoder-audiodecoder-mediastreamtrackgenerator.js
+++ audioencoder-audiodecoder-mediastreamtrackgenerator.js
@@ -1,201 +1,151 @@
-const audio = document.querySelector('audio');
-const button = document.querySelector('button');
-let music_buffer = void 0;
-function splitBuffer(buffer, length) {
-  let result = [];
-  let channels = [];
-  for (let i = 0; i < buffer.numberOfChannels; i++) {
-    channels.push(buffer.getChannelData(i));
-  }
-  for (let offset = 0; offset < buffer.length; offset += length) {
-    let len = Math.min(length, buffer.length - offset);
-    let small_buf = new AudioBuffer({
-      length: len,
-      numberOfChannels: buffer.numberOfChannels,
-      sampleRate: buffer.sampleRate,
-    });
-    for (let i = 0; i < buffer.numberOfChannels; i++) {
-      small_buf.copyToChannel(channels[i].slice(offset, offset + len), i);
-    }
-    result.push(small_buf);
-  }
-  return result;
-}
 async function main() {
-  audio.load();
-  audio.srcObject = null;
-  const generator = new MediaStreamTrackGenerator({ kind: 'audio' });
-  const { writable } = generator;
-  const writer = writable.getWriter();
-  const mediaStream = new MediaStream([generator]);
-  const outputCtx = new AudioContext({
-    sampleRate: 48000,
+  const ac = new AudioContext({
+    sampleRate: 22050,
     latencyHint: 0,
   });
-  await outputCtx.suspend();
-  let { sampleRate } = outputCtx;
-  let frameLength = sampleRate / 100;
-  let total_encoded_size = 0;
-  const overflow = [];
-  const {
-    readable: decoderReadable,
-    writable: decoderWritable,
-  } = new TransformStream();
-  const decoderReader = decoderReadable.getReader();
-  const decoderWriter = decoderWritable.getWriter();
+  const osc = new OscillatorNode(ac, { frequency: 0, channelCount: 1 });
+  const msd = new MediaStreamAudioDestinationNode(ac, {
+    channelCount: 1,
+  });
+  const { stream } = msd;
+  const [track] = stream.getAudioTracks();
+  const processor = new MediaStreamTrackProcessor({ track });
+  const generator = new MediaStreamTrackGenerator({ kind: 'audio' });
+  const { writable } = generator;
+  const { readable } = processor;
+  const audioWriter = writable.getWriter();
+  const mediaStream = new MediaStream([generator]);
+  const audio = document.querySelector('audio');
+  const total = document.getElementById('total');
+  const button = document.querySelector('button');
+  const config = {
+    numberOfChannels: 1,
+    sampleRate: 22050,
+    codec: 'opus',
+    bitrate: 16000,
+  };
+  const encoder = new AudioEncoder({
+    error(e) {
+      console.error(e);
+    },
+    output(chunk, metadata) {
+      if (metadata.decoderConfig) {
+        decoder.configure(metadata.decoderConfig);
+        console.log({ metadata });
+      }
+      total_encoded_size += chunk.byteLength;
+      total.textContent = 'Total encoded size: ' + total_encoded_size;
+      decoder.decode(chunk);
+    },
+  });
+  encoder.configure(config);
   const decoder = new AudioDecoder({
     error(e) {
       console.error(e);
     },
     async output(frame) {
-      let floats = frame.buffer.getChannelData(0);
-      // first assertion fails: 'AudioFrame.buffer.getChannelData(0).length: 2568'
-      // all subsequent AudioFrame.buffer.getChannelData(0).length: 2880
-      console.assert(floats.length === 2880, [
-        `AudioFrame.buffer.getChannelData(0).length: ${floats.length}`,
-      ]);
-      if (overflow.length) {
-        floats = new Float32Array([
-          ...overflow.splice(0, overflow.length),
-          ...floats,
-        ]);
-      }
-      for (let i = 0; i < floats.length; i += frameLength) {
-        // handle overflow, less than 480 (sampleRate/100); 440; 220
-        if (i + frameLength > floats.length) {
-          overflow.push(...floats.slice(i, floats.length));
-          break;
-        }
-        decoderWriter.write(floats.subarray(i, i + frameLength));
-      }
-      frame.close();
+      await audioWriter.write(frame);
     },
   });
-  const encoder = new AudioEncoder({
-    error(e) {
-      console.log(e);
-    },
-    output(chunk, metadata) {
-      total_encoded_size += chunk.data.byteLength;
-      document.getElementById('total').innerText =
-        'Total encoded size: ' + total_encoded_size;
-      if (metadata.decoderConfig) {
-        decoder.configure(metadata.decoderConfig);
-      }
-      decoder.decode(chunk);
-    },
-  });
-  const config = {
-    numberOfChannels: 1,
-    sampleRate,
-    codec: 'opus',
-    bitrate: 48000,
-  };
-  encoder.configure(config);
-  // ImperialMarch60.wav https://www2.cs.uic.edu/~i101/SoundFiles/
-  // https://bugs.chromium.org/p/chromium/issues/detail?id=1184070
-  // https://wc-audio-gen.glitch.me/
-  // 'https://cdn.glitch.com/f92b40ba-41b8-4076-a8c7-f66c1ccfd371%2Fmusic.wav?v=1616487361153'
-  let raw_music_wav = await fetch('./ImperialMarch60.webm');
-  if (!music_buffer)
-    music_buffer = await outputCtx.decodeAudioData(
-      await raw_music_wav.arrayBuffer()
-    );
-  let { duration } = music_buffer;
-  console.log(duration);
-  let recorder;
-  audio.ontimeupdate = async () => {
-    if (audio.currentTime >= duration) {
-      audio.ontimeupdate = null;
-      console.log(
-        `AudioBuffer.duration: ${duration}, HTMLAudioElement.currentTime: ${audio.currentTime},  AudioContext.currentTime: ${outputCtx.currentTime}`
-      );
+  audio.onpause = audio.onplay = audio.srcObject = null;
+  audio.srcObject = mediaStream;
+  let paused = false;
+  let paused_timer = 0;
+  let paused_time = 0;
+  audio.onpause = () => {
+    if (paused === false) {
+      paused = true;
+    }
+    if (audio.currentTime > 0) {
+      paused_timer = performance.now();
+    }
+    if (generator.readyState === 'ended') {
+      button.disabled = false;
+      button.textContent = button.textContent.slice(0, -3);
     }
   };
-  audio.onpause = () => {
-    audio.onpause = null;
+  audio.onplay = () => {
+    if (paused) {
+      paused_time += performance.now() - paused_timer;
+    }
+    if (audio.currentTime === 0) {
+      button.disabled = true;
+      button.textContent = button.textContent + 'ing';
+    }
+  };
+  osc.connect(msd);
+  osc.start();
+  let music_buffer;
+  let raw_music_wav = await fetch(
+    './ImperialMarch60.webm?=' + new Date().getTime()
+  );
+  if (!music_buffer)
+    music_buffer = await ac.decodeAudioData(await raw_music_wav.arrayBuffer());
+  let i = 0;
+  let total_encoded_size = 0;
+  let duration = 0;
+  let floats = music_buffer.getChannelData(0);
+  try {
+    await readable.pipeTo(
+      new WritableStream({
+        async write(value) {
+          const { timestamp } = value;
+          if (i < floats.length) {
+            let data = new Float32Array(220);
+            data.set(floats.subarray(i, i + 220));
+            i += 220;
+            const ad = new AudioData({
+              timestamp,
+              data,
+              numberOfChannels: 1,
+              numberOfFrames: 220,
+              sampleRate: 22050,
+              format: 'f32-planar',
+            });
+            duration += ad.duration;
+            encoder.encode(ad);
+          } else {
+            try {
+              if (paused === false) {
+                // play entire duration, Chromium ends track prematurely
+                // when audio is not paused with controls
+                // audio.currentTime should not be less than duration
+                await new Promise((resolve) =>
+                  setTimeout(
+                    resolve,
+                    (duration / 10 ** 6 - audio.currentTime) * 1000
+                  )
+                );
+              }
+              await encoder.flush();
+              await decoder.flush();
+              await audioWriter.close();
+              osc.stop();
+              track.stop();
+              generator.stop();
+              await audioWriter.closed;
+              return audioWriter.releaseLock();
+            } catch (err) {
+              throw err;
+            }
+          }
+        },
+        abort(e) {
+          console.log(e);
+        },
+      })
+    );
+    const { currentTime } = audio;
+    console.log(
+      'Done encoding, decoding, streaming audio.',
+      { duration, currentTime, pausedTime: paused_time / 1000 },
+      currentTime + paused_time / 1000
+    );
     button.disabled = false;
     button.textContent = button.textContent.slice(0, -3);
-  };
-  audio.onplay = () => {
-    audio.onplay = null;
-    recorder = new MediaRecorder(audio.srcObject);
-    recorder.start();
-    recorder.ondataavailable = ({ data }) =>
-      console.log(URL.createObjectURL(data));
-    button.disabled = true;
-    button.textContent = button.textContent + 'ing';
-  };
-  let buffers = splitBuffer(music_buffer, frameLength);
-  let base_time = 0;
-  for (let buffer of buffers) {
-    let frame = new AudioFrame({
-      timestamp: base_time * 1000000,
-      buffer: buffer,
-    });
-    base_time += buffer.duration;
-    encoder.encode(frame);
+
+    await ac.close();
+  } catch (err) {
+    console.error(err);
   }
-  await encoder.flush();
-  await decoder.flush();
-  console.log(
-    `WritableStreamDefaultWriter.desiredSize: ${decoderWriter.desiredSize}`
-  );
-  const msd = new MediaStreamAudioDestinationNode(outputCtx);
-  const [track] = msd.stream.getAudioTracks();
-  track.onended = track.onmute = track.onunmute = (e) => console.log(e);
-  const osc = new OscillatorNode(outputCtx, { frequency: 0 });
-  osc.start();
-  osc.connect(msd);
-  const processor = new MediaStreamTrackProcessor(track);
-  const { readable } = processor;
-  const reader = readable.getReader();
-  await outputCtx.resume();
-  audio.srcObject = mediaStream;
-  return reader
-    .read()
-    .then(async function stream({ value }) {
-      const { timestamp } = value;
-      const buffer = new AudioBuffer({
-        numberOfChannels: 1,
-        length: frameLength,
-        sampleRate,
-      });
-      const { duration: currentDuration } = buffer;
-      const { value: floats, done } = await decoderReader.read();
-      buffer.getChannelData(0).set(floats);
-      const frame = new AudioFrame({ timestamp, buffer });
-      return writer.write(frame).then(async () => {
-        if (decoderWriter.desiredSize === 0) {
-          await new Promise((resolve) =>
-            setTimeout(
-              resolve,
-              (currentDuration + duration - audio.currentTime) * 1000
-            )
-          );
-          console.log(
-            `WritableStreamDefaultWriter.desiredSize: ${decoderWriter.desiredSize}`,
-            audio.currentTime
-          );
-          await decoderWriter.close();
-          generator.stop();
-          writer.releaseLock();
-          await writable.close();
-          await writable.closed;
-          recorder.stop();
-          reader.cancel();
-          osc.stop();
-          osc.disconnect();
-          track.stop();
-          // pause event not fired
-          if (!audio.paused) {
-            audio.pause();
-          }
-          await outputCtx.close();
-          return reader.closed.then(() => 'Done encoding and decoding.');
-        }
-        return reader.read().then(stream);
-      });
-    })
-    .then(console.log, console.error);
 }
